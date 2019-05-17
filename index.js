4394531a7c907a6b634009c9ec5dae3f8c1266c1
Rick Battipede <rbattipede@gmail.com>
2019-05-17 01:54:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
- var array = kittens.slice(kittens.length - 1)
+ var array = kittens.slice(0,kittens.length - 1)
  return array;
 }
 
