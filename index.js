1c125982a86d6393b047a070c641a8504ff61e0e
Rick Battipede <rbattipede@gmail.com>
2019-05-17 01:18:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(){
   kittens.push("Ralph");
 }
+
+function destructivelyPrependKitten(){
+  
+}
+
+function destructivelyRemoveLastKitten(){
+  
+}
